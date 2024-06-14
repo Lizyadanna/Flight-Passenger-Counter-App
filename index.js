d1f20a4c8bede31d02668ea62c70c1b16c48f9c1
Lizy Agbakahi <lizyadannaagb@gmail.com>
2024-06-14 16:09:22 -0500

    incrememnt functionality added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.getElementById("count-el").innerText = 5;
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let passengerCount = 0;
+
+function incrementCount() {
+    passengerCount = passengerCount + 1
+    countEl.innerText = passengerCount;
+    console.log(passengerCount)
+}
